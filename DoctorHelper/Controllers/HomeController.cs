fe67407a94a83f4e183b99c10166e6e6f55f0edd
mrVengr <jocoala@mail.ru>
2018-06-03 15:39:51 +0300

    Login

--- DoctorHelper/Controllers/HomeController.cs
+++ DoctorHelper/Controllers/HomeController.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DoctorHelper.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DoctorHelper.Controllers
 {
     public class HomeController : Controller
     {
+        //[AllowAnonymous]
         public IActionResult Index()
         {
             return View();
@@ -26,6 +29,16 @@ namespace DoctorHelper.Controllers
         }
 
         public IActionResult Medicine()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
         {
             return View();
         }
--- DoctorHelper/Startup.cs
+++ DoctorHelper/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,7 +23,17 @@ namespace DoctorHelper
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDistributedMemoryCache();
+
             services.AddMvc();
+
+            //services.AddMvc(config =>
+            //{
+            //    var policy = new AuthorizationPolicyBuilder()
+            //                     .RequireAuthenticatedUser()
+            //                     .Build();
+            //    config.Filters.Add(new AuthorizeFilter(policy));
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
